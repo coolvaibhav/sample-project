2e40e92280cbbc85a89f83df90bb8d11ee5ae6e5
vaibhav kulkarni <vaibhav@loantap.in>
2018-11-13 16:21:55 +0530

    login page added

---
+++ login.php
@@ -0,0 +1,5 @@
+<html>
+	<input type="email" name="email" id="email" placeholder="Email"><br/>
+	<input type="password" name="password" id="password" placeholder="Password"><br/>
+	<input type="submit" value="Login" name="Submit">
+</html>
