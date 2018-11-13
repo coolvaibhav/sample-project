da3e2ff3fc2bf86f9c67ab45a935c2d8422e8253
vaibhav kulkarni <vaibhav@loantap.in>
2018-11-13 16:27:21 +0530

    register file is added

---
+++ register.php
@@ -0,0 +1,6 @@
+<html>
+	<input type="text" name="full_name"><br>
+	<input type="email" name="email"><br>
+	<input type="password" name="password"><br>
+	<input type="submit" value="register" name="register">
+<html>
